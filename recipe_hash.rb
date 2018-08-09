36ece60a709934659a4658281e163ec240c3d690
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-09 15:19:05 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -22,16 +22,16 @@ perfect_hash_recipe = {"almond flour" => "2 cups"
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
-perfect_10_recipe = {"almond flour" => 2 "cups", 
-   "gluten freewhole oats" => 1 "cup", 
-   "kosher salt" => 1/2 "tea spoon", 
-   "baking powder"  => 1/2 "tea spoon",
-   "baking soda" => 1/4 "tea spoon",
-   "Xanthan Gum" => 1/2 "tea spoon"
-   "slivered almonds, lightly toasted" => 1/4 "cup",
-   "mini dark chocolate chips" => 3/4 "cup",
-   "olive oil" => 1/2 "cup",
-   "agave" => 1/4 "cup"
+perfect_10_recipe = {:almond_flour => 2 "cups", 
+   :gluten_freewhole_oats => 1 "cup", 
+   :kosher_salt => 1/2 "tea spoon", 
+   :baking_powder  => 1/2 "tea spoon",
+   :baking_soda => 1/4 "tea spoon",
+   :Xanthan_Gum => 1/2 "tea spoon"
+   :slivered_almonds_lightly_toasted => 1/4 "cup",
+   :mini_dark_chocolate_chips => 3/4 "cup",
+   :olive_oil => 1/2 "cup",
+   :agave => 1/4 "cup"
 }
   puts perfect_10_recipe
 
